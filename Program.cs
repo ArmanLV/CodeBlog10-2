11d1d80f5ffa286a21b50a3194fc586df01e2e6a
ArmanLV <32594089+ArmanLV@users.noreply.github.com>
2019-04-21 15:50:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace CodeBlog11
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задаю значения скилла для экземпляров
+            int Skill1 = new Random().Next(0, 100);
+            int Skill2 = new Random().Next(0, 100);
+
+            // Задаю время зависимо от умений
+            int Time1 = SwimTime(Skill1);
+            int Time2 = SwimTime(Skill2);
+
+            // Объявляю экземпляры класса
+            Swimmer SwimmerOne = new Swimmer("Джугашвили", "кроль", Time1, 0, Skill1);
+            Swimmer SwimmerTwo = new Swimmer("Шикльгрубер", "кроль", Time2, 0, Skill2);
+
+            // Задаю значения разницы во времени (у победившего значение с +, а у проигравшего с -)
+            if (Time1 < Time2)
+            {
+                SwimmerOne.TimeDifference = Time2 - Time1;
+                SwimmerTwo.TimeDifference = -SwimmerOne.TimeDifference;
+            }
+            else
+            {
+                SwimmerTwo.TimeDifference = Time1 - Time2;
+                SwimmerOne.TimeDifference = -SwimmerTwo.TimeDifference;
+            }            
+
+            // Вывожу значения
+            Console.WriteLine($"{SwimmerOne.Time} {SwimmerOne.TimeDifference} {SwimmerOne.Skill}");
+            Console.WriteLine($"{SwimmerTwo.Time} {SwimmerTwo.TimeDifference} {SwimmerTwo.Skill}");
+            Console.ReadLine();
+        }
+
+        // Статический метод, который считает время зависимо от умений (Чем больше умения - меньше время) 
+        static int SwimTime(int skill)
+        {
+            if (skill < 20)
+            {
+                return new Random().Next(80, 100);
+            }
+            else if (skill > 20 && skill < 50)
+            {
+                return new Random().Next(45, 80);
+            }
+            else if (skill > 50 && skill < 80)
+            {
+                return new Random().Next(35, 50);
+            }
+            else
+            {
+                return new Random().Next(35, 50);
+            }
+        }
+    }
+}
---
+++ Swimmer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CodeBlog11
+{
+    public class Swimmer
+    {
+        // Объявляю общие параметры у пловцов
+        public string Name { get; private set; }
+        public string SwimmingWay { get; private set; }
+        public int Time { get; private set; }
+        public int TimeDifference { get; set; }
+        public int Skill { get; private set; }
+
+        // Базовый конструктор класса, который проверяет входные данные
+        public Swimmer(string name, string swimmingWay, int time, int timeDifference, int skill)
+        {
+            if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(swimmingWay))
+            {
+                throw new ArgumentNullException("Данное поле не может быть пустым");
+            }
+            if (time <= 0 )
+            {
+                throw new ArgumentException("Данный параметр не может быть равен или меньше нуля");
+            }
+            if (timeDifference < 0)
+            {
+                throw new ArgumentException("Данный параметр не может быть меньше нуля");
+            }
+            if (skill < 0 || skill > 100)
+            {
+                throw new ArgumentException("Данный параметр может принимать только значения от 0 до 100 включительно");
+            }
+
+            // Если входные данные проходят все проверки, то присваиваю значения экземпляра
+            Name = name;
+            SwimmingWay = swimmingWay;
+            Time = time;
+            TimeDifference = timeDifference;
+            Skill = skill;
+        }
+    }
+}
